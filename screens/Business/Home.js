0aa796fa22f03246ee52289451b0a4342d5956ba
Karim El Jazzar <jazzar.karim@gmail.com>
2022-12-15 01:06:48 -0500

    Hotfix home

--- screens/Business/Home.js
+++ screens/Business/Home.js
@@ -57,8 +57,8 @@ export default function Profile( { navigation } ) {
     }
 
     useEffect(() => {
-       getUserLocation()
-       getCarsDB()
+       //getUserLocation()
+       //getCarsDB()
     }, [uLocation]);
 
     useEffect(() => {
